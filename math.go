d2e9ed6ad4f9c922e3585100ebf3fc6c2d205a6f
David Rafael Cardoso <davidcardoso01@gmail.com>
2023-02-21 21:37:36 -0300

    feat: :sparkles: Adicionado funcao de soma novamente

--- math.go
+++ math.go
@@ -1,9 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
-	// fmt.Println(soma(10,10))
+	fmt.Println(soma(10, 10))
 }
 
-// func soma(a int, b int) int{
-// 	return a + b
-// }
+func soma(a int, b int) int {
+	return a + b
+}
